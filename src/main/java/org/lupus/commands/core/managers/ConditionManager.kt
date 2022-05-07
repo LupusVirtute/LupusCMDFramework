f560542325c8c401d73dcce75d080de006efbb45
PuccyDestroyerxXx <mrcake.2014@wp.pl>
2022-05-07 20:13:45 +0200

    :bug: fix(ConditionManager): override get operator to get only lower case options

--- src/main/java/org/lupus/commands/core/managers/ConditionManager.kt
+++ src/main/java/org/lupus/commands/core/managers/ConditionManager.kt
@@ -7,6 +7,12 @@ object ConditionManager : HashMap<String, ConditionFun>() {
 	init {
 		ConditionManager["creative"] = CreativeCond
 	}
+
+	override operator fun get(key: String): ConditionFun? {
+		val betterKey = key.lowercase()
+		return super.get(betterKey)
+	}
+
 	override fun put(key: String, value: ConditionFun): ConditionFun? {
 		val betterKey = key.lowercase()
 		return super.put(betterKey, value)
